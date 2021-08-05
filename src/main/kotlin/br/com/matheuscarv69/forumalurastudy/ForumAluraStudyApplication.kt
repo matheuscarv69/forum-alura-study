1592ccf6061a8d74e5431b40d155b71d4a9164dd
matheuscarv69 <matheus9126@gmail.com>
2021-08-05 10:10:01 -0400

    :tada: initial commit

---
+++ src/main/kotlin/br/com/matheuscarv69/forumalurastudy/ForumAluraStudyApplication.kt
@@ -0,0 +1,11 @@
+package br.com.matheuscarv69.forumalurastudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ForumAluraStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<ForumAluraStudyApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/matheuscarv69/forumalurastudy/ForumAluraStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.matheuscarv69.forumalurastudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForumAluraStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
